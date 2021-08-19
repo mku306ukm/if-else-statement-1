f1837a772741b3fbf98d5b359050777d4dca88c6
umesh_kumar_mehta <kumarumesh15294@gmail.com>
2021-08-19 00:16:59 -0700

    if-else-statement-1

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log('hi')
+
+const age = 15;
+if (age >= 18) {
+    console.log('sarah can start driving license' );
+
+}else{
+    const yearsLeft = 18-age;
+    console.log(`sarah is too young. wait another ${yearsLeft} years!`);
+document.write(`sarah is too young. wait another ${yearsLeft} years!`)
+document.write('<br><br>');
+
+}
+const birthyear = 2012;
+let century;
+if (birthyear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
+document.write(century);
